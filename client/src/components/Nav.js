4bae6aa1bc92d6a2a7577b62e4dbcb5326b622a4
Faareh-Ahmed <faarehahmed@gmail.com>
2023-09-30 23:43:17 +0500

    Nav and Hero Section Designed

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,29 +4,46 @@ import Lottie from 'lottie-react';
 import hamburger from '../icons/hamburger.svg'
 export default function Nav() {
   return (
-<>
-<header className='padding-x py-8 absolute z-10 w-full bg-gray-500'>
-        <nav className=' flex justify-between items-center max-container bg-purple-400'>
+    <>
+      <header className='padding-x py-8 absolute z-10 w-full'>
+        <nav className=' flex justify-between items-center max-container'>
 
 
-          <div className='w-1/12 h-1/6 bg-black'>
-            <Lottie animationData={Lottie1}  />
+          <div className='w-1/12 h-1/6 '>
+            <Lottie animationData={Lottie1} />
           </div>
 
           <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
-              <li>Home</li>
-              <li>Description</li>
-              <li>Contributors</li>
-              <li>Contact Us</li>
-              
+            <li>
+              <button className="bg-blue-600 text-white hover:bg-blue-300 hover:text-blue-600 py-2 px-6 rounded-full transition duration-300 ease-in-out transform hover:scale-105">
+                Home
+              </button>
+            </li>
+            <li>
+            <button className="bg-blue-600 text-white hover:bg-blue-300 hover:text-blue-600 py-2 px-6 rounded-full transition duration-300 ease-in-out transform hover:scale-105">
+                Description
+              </button>
+            </li>
+            
+            <li>
+            <button className="bg-blue-600 text-white hover:bg-blue-300 hover:text-blue-600 py-2 px-6 rounded-full transition duration-300 ease-in-out transform hover:scale-105">
+                Contact Us
+              </button>
+            </li>
+            <li>
+            <button className="bg-blue-600 text-white hover:bg-blue-300 hover:text-blue-600 py-2 px-6 rounded-full transition duration-300 ease-in-out transform hover:scale-105">
+                Login
+              </button>
+            </li>
+
           </ul>
           <div className='hidden max-lg:block'>
-            <img src={hamburger} alt='hamburger' height={25} width={25}/>
+            <img src={hamburger} alt='hamburger' height={35} width={35} />
           </div>
 
 
         </nav>
       </header>
-</>
-    )
+    </>
+  )
 }

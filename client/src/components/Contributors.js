086d6056a10bd50e8d8b00ada29829d7034fde47
Faareh-Ahmed <faarehahmed@gmail.com>
2023-10-06 08:59:34 +0500

    Added Button on Contributors

--- client/src/components/Contributors.js
+++ client/src/components/Contributors.js
@@ -23,15 +23,18 @@ export default function Contributors() {
 
       <div className="h-screen flex flex-col justify-center bg-red-400">
         {/* <h1 className="text-2xl font-bold mb-4">CONTRIBUTORS</h1> */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 h-1/2">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 h-1/2 bg-slate-500">
           {contributors.map((contributor) => (
-            <div key={contributor.username} className="bg-black rounded-lg p-4 shadow-md">
+            <div key={contributor.username} className="bg-white rounded-lg p-4 shadow-md">
               <img
                 src={contributor.avatarUrl}
                 alt={`${contributor.username}'s avatar`}
                 className="w-1/4 h-2/6 rounded-full mx-auto mb-2"
               />
-              <p className="text-center">{contributor.username}</p>
+
+              <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4  justify-center">
+                {contributor.username}
+              </button>
             </div>
           ))}
         </div>

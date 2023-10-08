fdf2700fe449b6d77568fbb31d595597bd588bfb
Faareh-Ahmed <faarehahmed@gmail.com>
2023-10-09 00:00:42 +0500

    Updated Styling for Contributor

--- client/src/components/Contributors.js
+++ client/src/components/Contributors.js
@@ -1,8 +1,6 @@
-import React from 'react'
-
+import React from 'react';
 
 export default function Contributors() {
-
   const contributors = [
     {
       username: 'Awais Nazir',
@@ -27,31 +25,33 @@ export default function Contributors() {
 
   return (
     <>
-      <h1 className="text-2xl font-bold mb-4">CONTRIBUTORS</h1>
+    <div className= "md:h-screen  bg-blue-200"  >
 
-      <div className="h-screen flex flex-col justify-center bg-red-400">
+    
+      <h1 className="text-2xl font-bold mb-4 ">CONTRIBUTORS</h1>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 h-1/2 bg-slate-500">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4  md:h-1/2 bg-lime-500">
+        {contributors.map((contributor) => (
+          <div
+            key={contributor.username}
+            className="bg-white rounded-lg p-4 shadow-md flex flex-col items-center "
+          >
+            <img
+              src={contributor.avatarUrl}
+              alt={`${contributor.username}'s avatar`}
+              className="w-1/4 h-2/6 rounded-full mx-auto mb-2"
+            />
+            <button
+              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 w-2/3"
+              onClick={() => openGitHubProfile(contributor.githubUrl)}
+            >
+              {contributor.username}
+            </button>
+          </div>
+        ))}
+      </div>
 
-          {contributors.map((contributor) => (
-            <div key={contributor.username} className="bg-white rounded-lg p-4 shadow-md flex flex-col items-center ">
-
-
-              <img
-                src={contributor.avatarUrl}
-                alt={`${contributor.username}'s avatar`}
-                className="w-1/4 h-2/6 rounded-full mx-auto mb-2"
-              />
-
-              <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 w-2/3"
-                onClick={() => openGitHubProfile(contributor.githubUrl)}
-              >
-                {contributor.username}
-              </button>
-            </div>
-          ))}
-        </div>
       </div>
     </>
-  )
+  );
 }

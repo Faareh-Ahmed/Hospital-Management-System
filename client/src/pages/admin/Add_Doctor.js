9314f561b69b2ac089a37e9c4ad6d8c352127c85
Faareh-Ahmed <faarehahmed@gmail.com>
2023-12-01 08:54:23 +0500

    Updated Form Inputs for several pages

--- client/src/pages/admin/Add_Doctor.js
+++ client/src/pages/admin/Add_Doctor.js
@@ -96,6 +96,20 @@ export default function Add_Doctor() {
                                             </div>
                                         </div>
 
+                                        <div className="sm:col-span-4">
+                                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                                Lisence Number
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    id="lisence-number"
+                                                    name="lisence-number"
+                                                    type="number"
+                                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                />
+                                            </div>
+                                        </div>
+
                                         <div className="col-span-full">
                                             <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
                                                 Specialization
@@ -127,7 +141,7 @@ export default function Add_Doctor() {
                                                     <option>Morning</option>
                                                     <option>Evening</option>
                                                     <option>Night</option>
-                                                    
+
                                                 </select>
                                             </div>
                                         </div>
@@ -163,99 +177,17 @@ export default function Add_Doctor() {
                                             </div>
                                         </div>
 
-                                        <div className="sm:col-span-2">
-                                            <label htmlFor="region" className="block text-sm font-medium leading-6 text-gray-900">
-                                                Weight (kg)
-                                            </label>
-                                            <div className="mt-2">
-                                                <input
-                                                    type="text"
-                                                    name="weight"
-                                                    id="weight"
-                                                    autoComplete="off"
-                                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                                    placeholder="Enter Weight"
-                                                />
-                                            </div>
-                                        </div>
+                   
 
-                                        <div className="sm:col-span-2">
-                                            <label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-gray-900">
-                                                ZIP / Postal code
-                                            </label>
-                                            <div className="mt-2">
-                                                <input
-                                                    type="text"
-                                                    name="postal-code"
-                                                    id="postal-code"
-                                                    autoComplete="postal-code"
-                                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                                />
-                                            </div>
-                                        </div>
+                                
                                     </div>
                                 </div>
 
                                 <div className="border-b border-gray-900/10 pb-12">
-                                    <h2 className="text-base font-semibold leading-7 text-gray-900">Notifications</h2>
-                                    <p className="mt-1 text-sm leading-6 text-gray-600">
-                                        We'll always let you know about important changes, but you pick what else you want to hear about.
-                                    </p>
+                                   
 
                                     <div className="mt-10 space-y-10">
-                                        <fieldset>
-                                            <legend className="text-sm font-semibold leading-6 text-gray-900">By Email</legend>
-                                            <div className="mt-6 space-y-6">
-                                                <div className="relative flex gap-x-3">
-                                                    <div className="flex h-6 items-center">
-                                                        <input
-                                                            id="comments"
-                                                            name="comments"
-                                                            type="checkbox"
-                                                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                                        />
-                                                    </div>
-                                                    <div className="text-sm leading-6">
-                                                        <label htmlFor="comments" className="font-medium text-gray-900">
-                                                            Comments
-                                                        </label>
-                                                        <p className="text-gray-500">Get notified when someones posts a comment on a posting.</p>
-                                                    </div>
-                                                </div>
-                                                <div className="relative flex gap-x-3">
-                                                    <div className="flex h-6 items-center">
-                                                        <input
-                                                            id="candidates"
-                                                            name="candidates"
-                                                            type="checkbox"
-                                                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                                        />
-                                                    </div>
-                                                    <div className="text-sm leading-6">
-                                                        <label htmlFor="candidates" className="font-medium text-gray-900">
-                                                            Candidates
-                                                        </label>
-                                                        <p className="text-gray-500">Get notified when a candidate applies for a job.</p>
-                                                    </div>
-                                                </div>
-                                                <div className="relative flex gap-x-3">
-                                                    <div className="flex h-6 items-center">
-                                                        <input
-                                                            id="offers"
-                                                            name="offers"
-                                                            type="checkbox"
-                                                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                                                        />
-                                                    </div>
-                                                    <div className="text-sm leading-6">
-                                                        <label htmlFor="offers" className="font-medium text-gray-900">
-                                                            Offers
-                                                        </label>
-                                                        <p className="text-gray-500">Get notified when a candidate accepts or rejects an offer.</p>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </fieldset>
+                                    
                                         <fieldset>
                                             <legend className="text-sm font-semibold leading-6 text-gray-900">Gender</legend>
                                             <div className="mt-6 space-y-6">

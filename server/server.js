4334cf2014b9d1d0790de6ca27d6688a1de4b76c
Malik Shahzaib <malikshahzaib7238@gmail.com>
2023-12-08 20:34:25 +0500

    receptionist most done and add appointment works

--- server/server.js
+++ server/server.js
@@ -512,6 +512,95 @@ app.post("/admin/add-admitroom", (req, res) => {
   );
 });
 
+app.post("/receptionist/add-appointment", (req, res) => {
+  console.log("Data has arrived at the backend");
+  console.log(req.body);
+
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const contactNumber = req.body.contactNumber;
+  const gender = req.body.gender;
+  const age = req.body.age;
+  const diesease = req.body.diesease;
+  const bloodGroup = req.body.bloodGroup;
+  const address = req.body.address;
+  const height = req.body.height;
+  const weight = req.body.weight;
+  const doctorID = req.body.doctorID;
+  const date = req.body.date;
+  const BMI = (weight / (height * height)) * 10000;
+  const username = generateRandomUsername();
+  const password = generateRandomPassword();
+
+  db.query(
+    "INSERT INTO CREDENTIALS (UserName, Password) VALUES (?, ?)",
+    [username, password],
+    (err, credentialResult) => {
+      if (err) {
+        console.log("Error inserting credentials:", err);
+        return res.status(500).json({ error: "Error inserting credentials" });
+      }
+
+      const credentialId = credentialResult.insertId;
+
+      db.query(
+        "INSERT INTO USER (FirstName, LastName, Email, ContactNumber, Role, idCredentials,  gender) VALUES (?, ?, ?, ?, ?, ?,?)",
+        [
+          firstName,
+          lastName,
+          email,
+          contactNumber,
+          "Patient",
+          credentialId,
+          gender,
+        ],
+        (err, userResult) => {
+          if (err) {
+            console.log("Error inserting user data:", err);
+            return res.status(500).json({ error: "Error inserting user data" });
+          }
+
+          const userId = userResult.insertId;
+
+          db.query(
+            "INSERT INTO patient (idUser, Age, BloodGroup, Disease, Height, BMI, Weight, Address) VALUES (?, ?, ?, ?,?,?,?,?)",
+            [userId, age, bloodGroup, diesease, height, BMI, weight, address],
+            (err, patientResult) => {
+              if (err) {
+                console.log("Error inserting staff data:", err);
+                return res
+                  .status(500)
+                  .json({ error: "Error inserting staff data" });
+              }
+
+              const patientId = patientResult.insertId;
+
+              db.query(
+                "INSERT INTO appointment (idPatient, idDoctor, AppointmentDate, Status) VALUES (?, ?, ?,?)",
+                [patientId, doctorID, date, "Due"],
+                (err, appointmentResult) => {
+                  if (err) {
+                    console.log("Error inserting appointment data:", err);
+                    return res
+                      .status(500)
+                      .json({ error: "Error inserting appointment data" });
+                  }
+
+                  console.log("Data inserted successfully into the database");
+                  res
+                    .status(200)
+                    .json({ message: "Data inserted successfully" });
+                }
+              );
+            }
+          );
+        }
+      );
+    }
+  );
+});
+
 app.get("/admin/show-doctors", (req, res) => {
   console.log("show doctor wali api ko call aagayi");
   // Query to fetch appointment data from the appointment table
@@ -527,6 +616,39 @@ app.get("/admin/show-doctors", (req, res) => {
     }
   });
 });
+
+app.get("/receptionist/show-doctors", (req, res) => {
+  console.log("show doctor wali api ko call aagayi");
+  // Query to fetch appointment data from the appointment table
+  const sql =
+    "select idDoctor, concat(FirstName, ' ', LastName) as Name from user natural join staff natural join doctor;";
+
+  // Use the connection pool to execute the query
+  db.query(sql, (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.json(results); // Send the appointment data as JSON to the frontend
+    }
+  });
+});
+
+
+app.get("/receptionist/show-appointments", (req, res) => {
+  // Query to fetch appointment data from the appointment table
+  const sql =
+    "select idDoctor, concat(FirstName, ' ', LastName) as Name from user natural join staff natural join doctor;";
+
+  // Use the connection pool to execute the query
+  db.query(sql, (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.json(results); // Send the appointment data as JSON to the frontend
+    }
+  });
+});
+
 
 app.get("/admin/show-nurses", (req, res) => {
   // Query to fetch appointment data from the appointment table
@@ -583,7 +705,7 @@ app.get("/admin/show-admitroom", (req, res) => {
 });
 
 app.get("/admin/show-history", (req, res) => {
-  console.log("history wali api ko call aagayi")
+  console.log("history wali api ko call aagayi");
   const sql = `
   SELECT
     v.idVisit,
@@ -617,7 +739,6 @@ WHERE
     }
   });
 });
-
 
 app.post("/nurse/show-room", (req, res) => {
   // Query to fetch appointment data from the appointment table
